24b69da463a8647410df260157b78272ec7b4271
Luis Cordova <cordoval@gmail.com>
2011-07-26 01:52:19 -0500

    created a test - passed, another cucumber seed?

--- Tests/Calculator/CalculatorTest.php
+++ Tests/Calculator/CalculatorTest.php
@@ -2,7 +2,29 @@
 
 namespace Cordova\Bundle\FormModelBundle\Tests\Calculator;
 
-class DefaultControllerTest extends \PHPUnit_Framework_TestCase
+use Cordova\Bundle\FormModelBundle\Calculator\Calculator;
+
+class CalculatorTest extends \PHPUnit_Framework_TestCase
 {
+    protected $calculator;
     
+    protected function setUp()
+    {
+        $this->calculator = new Calculator();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAllowForNumbersToBePushed() {
+
+        $num = 3;
+        $this->calculator->push($num);
+        $this->calculator->add();
+        $this->assertEquals($this->calculator->result(), $num);
+    }
+
+//    public function shouldOutputTotalResult() {
+//        $calculator->push($argument);
+//    }
 }
