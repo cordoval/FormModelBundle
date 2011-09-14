6c3bc3d37520ba990ffd67bb2c49cfccd2e646e9
Luis Cordova <cordoval@gmail.com>
2011-09-14 16:12:44 -0500

    show jbohn

--- Specs/views/IndexSpec.php
+++ Specs/views/IndexSpec.php
@@ -1,18 +1,20 @@
 <?php
-namespace Review;
+namespace Cordova\Bundle\FormModelBundle\Specs\views;
 
-// loading from pear
-use \PHPSpec\Context\Zend\View as ViewContext;
-use \PHPSpec\Context\Symfony2\View as ViewContext2;
+use \PHPSpec\Context\Symfony2\View;
 
-class DescribeIndex extends ViewContext2
+class DescribeIndex extends View
 {
     function itRendersTheSelectedVideo()
     {
         $video = \Mockery::mock(
             'Application_Model_Video', array('getName' => 'Revolution OS'));
-        $this->assign('video', $video);
-        $this->render();
-        $this->rendered->should->contain('Revolution OS');
+
+        $output = $this->render('CordovaFormModelBundle:Default:index.html.twig', array(
+            'video' => $video,
+        ));
+        print_r($output);
+        //die();
+        $output->should->contain('Revolution OS');
     }
 }
